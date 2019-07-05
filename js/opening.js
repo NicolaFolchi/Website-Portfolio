2d0f45c6c7ab10a7f912bd5c3075f890be44a042
Nicola Folchi <nico93@live.unc.edu>
2019-07-05 04:40:08 -0400

    Changes in website organization. Added temporary links. Added new home page. Removed google translate API.

--- js/opening.js
+++ js/opening.js
@@ -2,3 +2,21 @@ $(document).ready(function(){
 	
 	$("#opening_fadein").fadeIn(1000);
 });
+
+
+$(document).ready(function(){
+	changeFont();
+});
+
+function changeFont(){
+	var text = $("#my_name");
+	text.css("font", "22px Consolas");
+	text.css("float", "left");
+	text.css("color", "#86C232");
+}
+
+$("#my_name").hover(function(){
+	// var text = $("#my_name");
+	// text.css.animate("background-color", "yellow");
+	$("#my_name").css("background", "url(images/asfalt.png)");
+})
