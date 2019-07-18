27b9f6f2fd088db2e4e51c159f2dbfda25e9907d
Nicola Folchi <nico93@live.unc.edu>
2019-07-17 23:28:51 -0400

    Mobile adaptability now working in home page

--- js/opening.js
+++ js/opening.js
@@ -10,14 +10,26 @@ $(document).ready(function(){
 
 function changeFont(){
 	var text = $("#my_name");
-	text.css("font", "18px Consolas");
 	text.css("float", "left");
 	text.css("color", "#86C232");
-	text.css("padding-left", "160px");
+	if ($(window).width() > 759) {
+		text.css("padding-left", "160px");
+		text.css("font", "18px Consolas");
+	}
+	else{
+		text.css("padding-top", "25px");
+		text.css("font", "22px Consolas");
+	}
+	
 }
+$(window).resize(function(){
+	if ($(window).width() > 759) {
+		text.css("padding-left", "160px");
+	}
+});
 
 $("#my_name").hover(function(){
 	// var text = $("#my_name");
 	// text.css.animate("background-color", "yellow");
 	$("#my_name").css("background", "url(images/asfalt.png)");
-})
+});
