aaea9deffde9c95dfcbe9f780c0121f8006b3743
Nicola Folchi <nico93@live.unc.edu>
2019-08-14 19:18:43 -0400

    Temporary addition of a side project to utilize github's hosting service

---
+++ Back-end_Project/query_mgt.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE Quotes(
+--     ID INTEGER NOT NULL PRIMARY KEY,
+--     Quotes TEXT(255) NOT NULL
+-- );
+
+-- INSERT INTO Quotes (quotes) VALUES
+-- ('esta es la primera entry'),
+-- ('esta es las segunda entry');
+
+-- SELECT * FROM Quotes;
+
+INSERT INTO Quotes (quotes) VALUES
+('tercera entry y vamos bien juju');
