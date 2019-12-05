58781aa00c94016a29c8d754f0eeeb31e92bf478
Nicola Folchi <nico93@live.unc.edu>
2019-12-05 02:34:03 -0500

    updating project link

--- js/projectData.js
+++ js/projectData.js
@@ -2,7 +2,7 @@ const projects = [{
     id: 1,
     pName: "Nicola's 2048",
     pDescription: "I developed my own version of the 2048 game which has a tiny variation to it at the time of playing it.",
-    pImg: "../images/2048.png",
+    pImg: "../2048.png",
     pLink: "https://nicolafolchi.github.io/2048js/",
     pLastUpdated: "October 2019"
 }];
