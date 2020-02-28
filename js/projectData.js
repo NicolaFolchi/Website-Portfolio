ea88e1f9fcf185d174a87dd63db99e6b03595a80
Nicola Folchi <nico93@live.unc.edu>
2020-02-28 16:52:46 -0500

    fixing path for project image

--- js/projectData.js
+++ js/projectData.js
@@ -2,7 +2,7 @@ const projects = [{
     id: 1,
     pName: "Nicola's 2048",
     pDescription: "I developed my own version of the 2048 game which has a tiny variation to it at the time of playing it.",
-    pImg: "images/2048.png",
+    pImg: "/../images/2048.png",
     pLink: "https://nicolafolchi.github.io/2048js/",
     pLastUpdated: "October 2019"
 },
@@ -10,7 +10,7 @@ const projects = [{
      id: 2,
      pName: "Adagio",
      pDescription: "A web aplication developed to provide a space for both casual music listeners and music enthusiasts to share, rate, and review songs and albums with friends.",
-     pImg: "images/2048.png",
+     pImg: "/../images/2048.png",
      pLink: "https://github.com/NicolaFolchi/comp426-webapp-project",
      pLastUpdated: "December 2019"
  }
